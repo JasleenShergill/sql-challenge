f59dbbf3978430fb6e1b604839ecc33e712229ef
Jasleen Jasleen <jasleenshergill2509@gmail.com>
2024-03-05 02:05:10 -0500

    [updating files to github]

---
+++ SQL - Queries for Module 9.sql
@@ -0,0 +1,87 @@
+-- Data Analysis (20 points)
+-- List the employee number, last name, first name, sex, and salary of each employee (2 points)
+
+SELECT e.emp_no AS "Employee Number",e.last_name AS "Last Name",
+		e.first_name AS "First Name", e.sex, s.salary AS "Salary"
+FROM employees AS e
+INNER JOIN salaries AS s
+ON e.emp_no = s.emp_no 
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986 (2 points)
+
+SELECT first_name AS "First Name" , last_name AS "Last Name",
+	   hire_date AS "Hire Date"
+FROM employees
+WHERE hire_date LIKE '%1986'
+ORDER BY emp_no;
+		
+-- List the manager of each department along with their department number, 
+--department name, employee number, last name, and first name (2 points)
+
+SELECT de.dept_name AS "Department Name", d.emp_no AS "Employee No.",
+		e.last_name AS "Last Name" , e.first_name As "First Name"
+FROM dept_manager AS d
+JOIN employees AS e
+ON d.emp_no = e.emp_no
+JOIN departments AS de
+ON d.dept_no = de.dept_no
+ORDER BY "Department Name" ASC ;
+
+-- List the department number for each employee along with that employee’s employee number,
+--last name, first name, and department name (2 points)
+
+SELECT d.dept_no AS "Department No.", d.emp_no AS "Employee No.",
+		e.last_name AS "Last Name" , e.first_name As "First Name",
+		de.dept_name AS "Department Name"
+FROM employees AS e
+JOIN dept_manager AS d
+ON d.emp_no = e.emp_no
+JOIN departments AS de
+ON d.dept_no = de.dept_no
+ORDER BY "Department Name" ASC ;
+
+-- List first name, last name, and sex of each employee whose first name is 
+--Hercules and whose last name begins with the letter B (2 points)
+
+SELECT first_name AS "First Name", last_name AS "Last Name",
+		sex as " Sex"
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- List each employee in the Sales department, including their employee number,
+--last name, and first name (2 points)
+
+SELECT emp_no AS "Employer Number",last_name AS "Last Name", first_name AS "First Name"
+FROM employees 
+WHERE emp_no IN
+(
+	SELECT emp_no
+	FROM dept_manager 
+	WHERE dept_no IN
+	(
+		SELECT dept_no
+		FROM departments 
+		WHERE dept_name = 'Sales'
+	)
+);
+
+-- List each employee in the Sales and Development departments, including their employee number,
+--last name, first name, and department name (4 points)
+
+
+SELECT e.emp_no AS "Employer Number",e.last_name AS "Last Name", 
+		e.first_name AS "First Name" ,d.dept_name AS "Department Name"
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no= de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales','Development');
+
+-- List the frequency counts, in descending order, of all the employee last names 
+--(that is, how many employees share each last name) (4 points)
+
+SELECT last_name AS "Last Name" , count(last_name) AS "Frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
---
+++ Table_Schema.sql
@@ -0,0 +1,102 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/h5Cmeh
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Data Engineering
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
+    "birth_date" DATE NOT NULL ,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+SELECT *
+FROM departments;
+
+SELECT *
+FROM dept_emp;
+
+SELECT *
+FROM dept_manager;
+
+SELECT *
+FROM employees;
+
+SELECT *
+FROM salaries;
+
+SELECT *
+FROM titles;
+
+ALTER TABLE employees
+	ALTER birth_date 
+	TYPE VARCHAR ;
+	
+ALTER TABLE employees
+	ALTER hire_date 
+	TYPE VARCHAR;	
+
+
